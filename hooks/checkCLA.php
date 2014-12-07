7d49c00fcc5ce9446caedd1ab72f6305c443146d
Jess Portnoy <jess.portnoy@kaltura.com>
2014-12-07 08:48:06 -0500

    there is a 1000 status setting limitation so, check if the stat is the same as prev stat, and if so, do not update

--- hooks/checkCLA.php
+++ hooks/checkCLA.php
@@ -1,6 +1,5 @@
 #!/usr/bin/php
 <?php
-// an example hook to integrat with this CLA system [https://github.com/kaltura/agent-contrib]
 require_once('/opt/git_hooks/github-php-client/client/GitHubClient.php');
 require_once('/opt/git_hooks/config.inc');
 define ('DBFILE','/opt/cla/db/cla.db');
@@ -11,13 +10,14 @@ $repo_name=SQLite3::escapeString($argv[2]);
 $pull_id=SQLite3::escapeString($argv[3]);
 $username=SQLite3::escapeString($argv[4]);
 $user_id=SQLite3::escapeString($argv[5]);
-// The URL you'd like to include in the status [see https://developer.github.com/v3/repos/statuses]
-// probably that of your CLA system
-$url="";
-// The message requesting the user to sign, this will appear as a comment in the pull request
-$sign_msg="Thank you for contributing this pull request!\nPlease sign the CLA so we can review and merge your contribution.\n";
+echo "$repo_name, $pull_id, $username, $user_id\n";
+$url="https://agentcontribs.kaltura.org";
+$sign_msg="Thank you for contributing this pull request!\nPlease sign the Kaltura CLA so we can review and merge your contribution.\nLearn more at http://bit.ly/KalturaContrib";
 $client = new GitHubClient();
+//$client->setDebug(true);
 $client->setCredentials(GITHUB_USER,GITHUB_PASSWD);
+$cla_checked=null;
+
 try{
 	$client->orgs->members->responseIfRequesterIsNotAnOrganizationMember($org,$username);
 }catch(exception $e){
@@ -32,6 +32,8 @@ try{
 		    die ($db->lastErrorCode() . ' '.$db->lastErrorMsg().' :(');
 		}
 		$res = $result->fetchArray(SQLITE3_ASSOC);
+		$myst= $client->repos->statuses->getCombinedStatus($org, $repo_name, $sha);
+		$state=$myst->getState();
 		if(!isset($res['name'])){
 			$cla_checked=false;
 			$statuses=$client->repos->statuses->listStatusesForSpecificRef($org, $repo_name,$sha);
@@ -41,13 +43,23 @@ try{
 				}
 			}
 			if (!$cla_checked){
+				//echo "Would have commented\n";
 				$client->comments->createComment($org, $repo_name, $pull_id, "Hi @".$username.",\n$sign_msg");
+				$cla_checked=true;
 			}
-			$client->repos->statuses->createStatus($org, $repo_name, $sha, 'pending',$url,"Pending for $username to sign CLA",'CLA');
-			echo "set $pull_id on $repo_name by $username to pending status\n";
+			if ($state!=='pending'){
+				$client->repos->statuses->createStatus($org, $repo_name, $sha, 'pending',$url,"Pending for $username to sign CLA",'CLA');
+				echo "set $pull_id on $repo_name by $username to pending status\n";
+			}else{
+				echo "Status for $pull_id on $repo_name by $username is $state\n";
+			}
 		}else{
-			$client->repos->statuses->createStatus($org, $repo_name, $sha, 'success',$url,$username. ' signed the CLA, request can be merged.','CLA');
-			echo "set $pull_id on $repo_name by $username to success status\n";
+			if ($state!=='success'){
+				$client->repos->statuses->createStatus($org, $repo_name, $sha, 'success',$url,$username. ' signed the CLA, request can be merged.','CLA');
+				echo "set $pull_id on $repo_name by $username to success status\n";
+			}else{
+				echo "Status for $pull_id on $repo_name by $username is $state\n";
+			}
 		}
 	}
 }
